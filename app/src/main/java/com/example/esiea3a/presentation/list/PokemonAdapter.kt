0d00c9ff8d7ec77e6b02c16a9d4cd50bc6b91456
EdMkn <63813031+EdMkn@users.noreply.github.com>
2021-05-23 00:24:12 +0200

    Fragment

--- app/src/main/java/com/example/esiea3a/presentation/list/PokemonAdapter.kt
+++ app/src/main/java/com/example/esiea3a/presentation/list/PokemonAdapter.kt
@@ -1,5 +1,10 @@
 package com.example.esiea3a.presentation.list
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.esiea3a.R
 
 class PokemonAdapter(private val dataSet: Array<String>) :
     RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
@@ -13,7 +18,7 @@ class PokemonAdapter(private val dataSet: Array<String>) :
 
         init {
             // Define click listener for the ViewHolder's View.
-            textView = view.findViewById(R.id.textView)
+            textView = view.findViewById(R.id.pokemon_name)
         }
     }
 
@@ -21,7 +26,7 @@ class PokemonAdapter(private val dataSet: Array<String>) :
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         // Create a new view, which defines the UI of the list item
         val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.text_row_item, viewGroup, false)
+            .inflate(R.layout.pokemon_item, viewGroup, false)
 
         return ViewHolder(view)
     }
--- app/src/main/java/com/example/esiea3a/presentation/list/PokemonListFragment.kt
+++ app/src/main/java/com/example/esiea3a/presentation/list/PokemonListFragment.kt
@@ -1,4 +1,4 @@
-package com.example.esiea3a
+package com.example.esiea3a.presentation.list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,18 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.esiea3a.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class PokemonListFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_pokemon_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
