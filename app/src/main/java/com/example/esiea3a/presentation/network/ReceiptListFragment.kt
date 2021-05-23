074616c4fb8dbc83dd96bef4082be215cd194942
EdMkn <63813031+EdMkn@users.noreply.github.com>
2021-05-23 20:13:32 +0200

    modif http 1

--- app/src/main/java/com/example/esiea3a/presentation/network/ReceiptListFragment.kt
+++ app/src/main/java/com/example/esiea3a/presentation/network/ReceiptListFragment.kt
@@ -1,5 +1,6 @@
-package com.example.esiea3a.presentation.data
+package com.example.esiea3a.presentation.network
 
+import android.app.Service
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,17 +9,23 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.esiea3a.R
+import com.example.esiea3a.presentation.data.Receipt
+import com.example.esiea3a.presentation.ui.ReceiptAdapter
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
+const val BASEURL = "https://jsonplaceholder.typicode.com/"
+
 class ReceiptListFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private val adapter = ReceiptAdapter(listOf())
     private val layoutManager = LinearLayoutManager(context)
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_receipt_list, container, false)
@@ -32,11 +39,19 @@ class ReceiptListFragment : Fragment() {
             layoutManager = this@ReceiptListFragment.layoutManager
             adapter = this@ReceiptListFragment.adapter
         }
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .build()
+        val receiptApi:ReceiptApi = retrofit.create(ReceiptApi::class.java)
+
+        receiptApi.get
+
         val bookList: ArrayList<Receipt> = arrayListOf<Receipt>().apply {
-            add(Receipt(1, title = "pistache", description = "faites"))
-            add(Receipt(2, title = "ndole", description = "un bon ndole"))
-            add(Receipt(3, title = "hero", description = "bon hero"))
-            add(Receipt(4, title = "mitumba", description = "bien malaxé"))
+            add(Receipt(1, title = "pistache", description = "faites",url=""))
+            add(Receipt(2, title = "ndole", description = "un bon ndole",url=""))
+            add(Receipt(3, title = "hero", description = "bon hero",url=""))
+            add(Receipt(4, title = "mitumba", description = "bien malaxé",url=""))
 
 
         }
--- app/src/main/java/com/example/esiea3a/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/esiea3a/presentation/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.esiea3a
+package com.example.esiea3a.presentation.ui
 
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.esiea3a.R
 
 class MainActivity : AppCompatActivity() {
 
